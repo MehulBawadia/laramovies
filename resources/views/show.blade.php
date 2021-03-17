5574799fcfc9cf29d352c393773d98aaea62d569
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-17 17:25:53 +0530

    Made responsive

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,13 +8,13 @@
     <div class="border-b border-gray-800">
         <div class="container mx-auto">
             <div class="px-8 py-16">
-                <div class="flex">
-                    <img src="{{ asset('/images/parasite.jpg') }}" alt="Parasite" class="w-96" />
+                <div class="flex flex-col md:flex-row">
+                    <img src="{{ asset('/images/parasite.jpg') }}" alt="Parasite" class="block mx-auto w-64 lg:w-96" />
 
-                    <div class="ml-24">
+                    <div class="md:ml-24 mt-8 md:mt-0">
                         <h1 class="text-4xl font-bold">Parasite (2019)</h1>
 
-                        <div class="flex items-center text-gray-400 text-sm mt-2">
+                        <div class="flex flex-wrap items-center text-gray-400 text-sm mt-2">
                             <svg class="fill-current text-yellow-600 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
                             <span class="ml-1">85%</span>
                             <span class="mx-2">|</span>
