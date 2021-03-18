21a76ccff93f55259dadb77021173241e523b921
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-18 16:31:40 +0530

    Added alpinejs

--- resources/views/partials/_layout.blade.php
+++ resources/views/partials/_layout.blade.php
@@ -16,6 +16,7 @@
 
     @include('partials._footer')
 
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <livewire:scripts>
 </body>
 </html>
