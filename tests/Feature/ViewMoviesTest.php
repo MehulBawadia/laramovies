478585d8231a80b80a3edce2de6bdc1795224dfd
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-18 13:49:46 +0530

    Testing the API for soingle movie response

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -29,6 +29,22 @@ class ViewMoviesTest extends TestCase
         $response->assertSeeText('Now Playing Fake Movie');
     }
 
+    /** @test */
+    public function the_movie_details_page_shows_correct_info()
+    {
+        $this->withoutExceptionHandling();
+
+        Http::fake([
+            config('services.tmdb.base_url') . '/movie/*' => $this->fakeSingleMovie(),
+        ]);
+
+        $response = $this->get(route('movies.show', 12345));
+        $response->assertSee('Fake Jumanji');
+        $response->assertSee('Jeanne McCarthy');
+        $response->assertSee('Casting Director');
+        $response->assertSee('Dwayne Johnson');
+    }
+
     private function fakePopularMovies()
     {
         return Http::response([
@@ -162,6 +178,85 @@ class ViewMoviesTest extends TestCase
                   "id" => 37,
                   "name" => "Western"
                 ],
+            ]
+        ], 200);
+    }
+
+    public function fakeSingleMovie()
+    {
+        return Http::response([
+            "adult" => false,
+            "backdrop_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+            "genres" => [
+                ["id" => 28, "name" => "Action"],
+                ["id" => 12, "name" => "Adventure"],
+                ["id" => 35, "name" => "Comedy"],
+                ["id" => 14, "name" => "Fantasy"],
+            ],
+            "homepage" => "http://jumanjimovie.com",
+            "id" => 12345,
+            "overview" => "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.",
+            "poster_path" => "/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg",
+            "release_date" => "2019-12-04",
+            "runtime" => 123,
+            "title" => "Fake Jumanji: The Next Level",
+            "vote_average" => 6.8,
+            "credits" => [
+                "cast" => [
+                    [
+                        "cast_id" => 2,
+                        "character" => "Dr. Smolder Bravestone",
+                        "credit_id" => "5aac3960c3a36846ea005147",
+                        "gender" => 2,
+                        "id" => 18918,
+                        "name" => "Dwayne Johnson",
+                        "order" => 0,
+                        "profile_path" => "/kuqFzlYMc2IrsOyPznMd1FroeGq.jpg",
+                    ]
+                ],
+                "crew" => [
+                    [
+                        "credit_id" => "5d51d4ff18b75100174608d8",
+                        "department" => "Production",
+                        "gender" => 1,
+                        "id" => 546,
+                        "job" => "Casting Director",
+                        "name" => "Jeanne McCarthy",
+                        "profile_path" => null,
+                    ]
+                ]
+            ],
+            "videos" => [
+                "results" => [
+                    [
+                        "id" => "5d1a1a9b30aa3163c6c5fe57",
+                        "iso_639_1" => "en",
+                        "iso_3166_1" => "US",
+                        "key" => "rBxcF-r9Ibs",
+                        "name" => "JUMANJI: THE NEXT LEVEL - Official Trailer (HD)",
+                        "site" => "YouTube",
+                        "size" => 1080,
+                        "type" => "Trailer",
+                    ]
+                ]
+            ],
+            "images" => [
+                "backdrops" => [
+                    [
+                        "aspect_ratio" => 1.7777777777778,
+                        "file_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+                        "height" => 2160,
+                        "iso_639_1" => null,
+                        "vote_average" => 5.388,
+                        "vote_count" => 4,
+                        "width" => 3840,
+                    ]
+                ],
+                "posters" => [
+                    [
+
+                    ]
+                ]
             ]
         ], 200);
     }
