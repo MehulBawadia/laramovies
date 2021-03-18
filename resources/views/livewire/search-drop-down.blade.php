73f67cd10cb814d924268f4e5fce6b2e130b7526
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-18 17:12:11 +0530

    Search via forward slash

--- resources/views/livewire/search-drop-down.blade.php
+++ resources/views/livewire/search-drop-down.blade.php
@@ -1,5 +1,10 @@
 <div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
-    <input wire:model.debouce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 pl-8 py-1 focus:outline-none focus:ring" placeholder="Search..." @focus="isOpen = true" @keydown="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" />
+    <input wire:model.debouce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 pl-8 py-1 focus:outline-none focus:ring" placeholder="Search..." @focus="isOpen = true" @keydown="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" x-ref="search" @keydown.window="
+        if (event.keyCode == 191) {
+            event.preventDefault();
+            $refs.search.focus();
+        }
+    " />
 
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-100 w-4" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
