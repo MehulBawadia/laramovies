9f31be0cf22716cc3778d22b0ee5b1cbbda46dc2
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-18 17:06:36 +0530

    Images in a modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -84,7 +84,7 @@
         </div>
     </div>
 
-    <div class="border-b border-gray-800">
+    <div class="border-b border-gray-800" x-data="{isOpen: false, image: ''}">
         <div class="container mx-auto">
             <div class="px-8 py-16">
                 <h2 class="text-3xl font-bold">Images</h2>
@@ -93,13 +93,39 @@
                     @foreach ($movie['images']['backdrops'] as $image)
                         @if ($loop->index < 9)
                             <div class="mt-8">
-                                <a href="#">
-                                    <img src="https://image.tmdb.org/t/p/w500/{{ $image['file_path'] }}" alt="{{ $image['file_path'] }}" class="hover:opacity-75 transition ease-in-out duration-150" />
+                                <a
+                                    @click.prevent="
+                                        isOpen = true
+                                        image='{{ 'https://image.tmdb.org/t/p/original/'.$image['file_path'] }}'
+                                    "
+                                    href="#"
+                                >
+                                    <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="{{ $image['file_path'] }}" class="hover:opacity-75 transition ease-in-out duration-150" />
                                 </a>
                             </div>
                         @endif
                     @endforeach
                 </div>
+
+                <div
+                    style="background-color: rgba(0, 0, 0, .5);"
+                    class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                    x-show="isOpen"
+                >
+                    <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                        <div class="bg-gray-900 rounded">
+                            <div class="flex justify-end pr-4 pt-2">
+                                <button
+                                    @click="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="text-3xl leading-none hover:text-gray-300">&times;
+                                </button>
+                            </div>
+                            <div class="modal-body px-8 py-8">
+                                <img :src="image" alt="poster">
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     </div>
