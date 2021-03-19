9de34bfdf26615579a5741660cae1958398ec161
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-19 13:36:54 +0530

    Abort if no / end of content

--- app/Http/Controllers/ActorsController.php
+++ app/Http/Controllers/ActorsController.php
@@ -9,6 +9,9 @@ class ActorsController extends Controller
 {
     public function index($pageNumber = 1)
     {
+        // 204 is http staus code for no content or end of content
+        abort_if($pageNumber > 500, 204);
+
         $popularActors = Http::withToken(config('services.tmdb.token'))
                             ->get(config('services.tmdb.base_url') . '/person/popular?page=' . $pageNumber)
                             ->json()['results'];
